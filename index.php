d727bece4f129cc71e3a81d298e1ef89993d4689
Makarov1142 <35515181+Makarov1142@users.noreply.github.com>
2018-01-17 15:07:20 +0700

    Update index.php

--- index.php
+++ index.php
@@ -17,9 +17,9 @@ $message_to_reply = '';
  * Some Basic rules to validate incoming messages
  */
 
-$api_key="<mLAP API KEY>";
-$url = 'https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'';
-$json = file_get_contents('https://api.mlab.com/api/1/databases/duckduck/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$message.'"}');
+$api_key="pa4iX2vuy7y_rf5uA4jrDw8cW2de3xVj";
+$url = 'https://api.mlab.com/api/1/databases/unknowdb/collections/linebot?apiKey='.$api_key.'';
+$json = file_get_contents('https://api.mlab.com/api/1/databases/chatbot/collections/linebot?apiKey='.$api_key.'&q={"question":"'.$message.'"}');
 $data = json_decode($json);
 $isData=sizeof($data);
 if (strpos($message, 'สอนเป็ด') !== false) {
